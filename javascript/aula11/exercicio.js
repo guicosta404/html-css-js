db384ee80c3c1ce8f78dd0f2e8c88c756a367599
Luiz Guilherme <lu.costa1402@gmail.com>
2026-09-24 19:42:55 -0300

    Praticando variaveis e constantes

---
+++ javascript/aula11/exercicio.js
@@ -0,0 +1,14 @@
+let varA = "A";
+let varB = "B";
+let varC = "C";
+
+// const utilA = varA
+
+// varA = varB
+// varB = varC
+// varC = utilA
+
+// Forma moderna
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
