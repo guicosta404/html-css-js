7ff9ddb31d09145a5996c4b0626a8d9e93946bc1
Luiz Guilherme <lu.costa1402@gmail.com>
2026-09-24 16:18:12 -0300

    let

---
+++ javascript/aula6/index.js
@@ -0,0 +1,16 @@
+// let nome = "João";
+
+
+// console.log(nome, "nasceu em 1984");
+// console.log("Em 2000", nome, "conheceu Maria.");
+// console.log(nome, "casou-se com Maria em 2012.");
+// console.log("Maria teve 1 filho com", nome, "em 2015.");
+// console.log("O filho de", nome, "se chama Eduardo.");
+
+// Não podemos criar variáveis com palavras reservadas
+// Não podem conter espaços ou traços
+// utilizamos camelCase
+// Não podemos redeclarar variáveis com let
+// Não utilizar var, utilize let
+
+let nomeCliente = 'Luiz'
