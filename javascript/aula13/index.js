96eca4d664aa69a700b37a95e2c053087a34c230
Luiz Guilherme <lu.costa1402@gmail.com>
2026-09-24 23:23:51 -0300

    Strings

---
+++ javascript/aula13/index.js
@@ -0,0 +1,14 @@
+let umaString = "Um texto";
+
+console.log(umaString.charAt(6)); // retorna o char no index desejado
+
+console.log(umaString.indexOf("texto"));
+
+console.log(umaString.length)
+
+console.log(umaString.slice(3,6))
+
+console.log(umaString.split(' '))
+
+console.log(umaString.toUpperCase())
+console.log(umaString.toLowerCase())
